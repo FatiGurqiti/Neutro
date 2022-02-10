b9ec5d19747d04e7e0f81ace128b6d3e0425e661
fatigurqiti <fati.gurciti1@gmail.com>
2022-02-10 23:54:22 +0100

    design change

--- app/src/main/java/com/fdev/ode/MainActivity.kt
+++ app/src/main/java/com/fdev/ode/MainActivity.kt
@@ -16,12 +16,59 @@ class MainActivity : AppCompatActivity() {
 
         val profile = findViewById<ImageButton>(R.id.profilebtn)
         val blackfilter = findViewById<ImageView>(R.id.blackfilter)
+        val Secondblackfilter = findViewById<ImageView>(R.id.secondblackfilter)
         val contactCard = findViewById<CardView>(R.id.addcontactCard)
         val ContactBtn = findViewById<ImageButton>(R.id.contactBtn)
+        val CancelContact = findViewById<ImageButton>(R.id.cancelContact)
         val AddContactBtn = findViewById<Button>(R.id.addContactBtn)
+
         val debtbtn = findViewById<ImageButton>(R.id.addDebtBtn)
+        val debtCard = findViewById<CardView>(R.id.addDebtCard)
+        val CanceldebtCard = findViewById<ImageButton>(R.id.cancelDebtCard)
+        val ContactlistCard = findViewById<CardView>(R.id.ContactListCard)
+        val CancelContactList = findViewById<ImageButton>(R.id.cancelContactList)
+        val amountText = findViewById<EditText>(R.id.AmountText)
+        val labelText = findViewById<EditText>(R.id.LabelText)
+        val dropDown = findViewById<ImageButton>(R.id.dropDownBtn)
+        val Contacttext = findViewById<EditText>(R.id.contactText);
         val progressBar = findViewById<ProgressBar>(R.id.progressBarinMainActivity)
         val odenumber = findViewById<EditText>(R.id.AddOdeNumber)
+
+
+        CancelContactList.setOnClickListener(){
+            progressBar.visibility = View.VISIBLE
+            Secondblackfilter.visibility = View.INVISIBLE
+            ContactlistCard.visibility = View.INVISIBLE
+            amountText.isEnabled = true
+            labelText.isEnabled = true
+            progressBar.visibility = View.INVISIBLE
+        }
+
+        debtbtn.setOnClickListener(){
+            progressBar.visibility = View.VISIBLE
+            blackfilter.visibility = View.VISIBLE
+            debtCard.visibility = View.VISIBLE
+            progressBar.visibility = View.INVISIBLE
+        }
+
+        dropDown.setOnClickListener(){
+            Contacttext.setText("Tihulu")
+            Secondblackfilter.visibility = View.VISIBLE
+            ContactlistCard.visibility = View.VISIBLE
+
+            amountText.isEnabled = false
+            labelText.isEnabled = false
+        }
+
+        CanceldebtCard.setOnClickListener(){
+            progressBar.visibility = View.VISIBLE
+            blackfilter.visibility = View.INVISIBLE
+            Contacttext.setText("")
+            amountText.setText("")
+            labelText.setText("")
+            debtCard.visibility = View.INVISIBLE
+            progressBar.visibility = View.INVISIBLE
+        }
 
         ContactBtn.setOnClickListener() {
             progressBar.visibility = View.VISIBLE
@@ -43,9 +90,18 @@ class MainActivity : AppCompatActivity() {
                 progressBar.visibility = View.VISIBLE
                 blackfilter.visibility = View.INVISIBLE
                 contactCard.visibility = View.INVISIBLE
+                odenumber.setText("")
                 progressBar.visibility = View.INVISIBLE
             }
 
+        }
+
+        CancelContact.setOnClickListener(){
+            progressBar.visibility = View.VISIBLE
+            blackfilter.visibility = View.INVISIBLE
+            contactCard.visibility = View.INVISIBLE
+            odenumber.setText("")
+            progressBar.visibility = View.INVISIBLE
         }
 
 
